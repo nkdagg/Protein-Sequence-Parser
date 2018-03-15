665821349e45a13aa1dbaba71c16e2c2f2c709c7
nk <nikita.pavlenko@ucdconnect.ie>
2018-03-15 03:27:35 +0000

    added new writeToFile function to make 4 datasets for 4 fold cross validation

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -125,16 +125,165 @@ public class Parser {
         }
     }
 
-    void writeToFile(boolean crossValidation) {
+    void writeToFile(boolean createFourFoldCrossValidationSets) {
+        // Creates 4 datasets for 4 fold cross-validation placed in respective src/fold folders.
 
+        try {
+            // In file:
+            // Line 1: # of examples
+            // Line 2: # of legal symbols, # of classes
+            int entriesForTraining = (entries.size() - entries.size() / 4);
+            int entriesForTesting = entries.size() / 4;
+            System.out.println("Total number of entries:" + entries.size());
+            System.out.println("Number of entries for training :" + entriesForTraining);
+            System.out.println("Number of entries for testing  :" + entriesForTesting);
+
+
+
+            // Init Fold 1 Writer
+            BufferedWriter wrTrain = new BufferedWriter(new FileWriter("src/fold1/train.dataset"));
+            BufferedWriter wrTest = new BufferedWriter(new FileWriter("src/fold1/test.dataset"));
+
+            wrTrain.write((Integer.toString(entriesForTraining))); // Number of training examples
+            wrTrain.newLine();
+            wrTrain.write("22 " + getNumberOfClasses());
+            wrTrain.newLine();
+
+            wrTest.write((Integer.toString(entriesForTesting))); // Number of test examples
+            wrTest.newLine();
+            wrTest.write("22 " + getNumberOfClasses());
+            wrTest.newLine();
+
+            // Init Fold 2 Writer
+            BufferedWriter wrTrain2 = new BufferedWriter(new FileWriter("src/fold2/train.dataset"));
+            BufferedWriter wrTest2 = new BufferedWriter(new FileWriter("src/fold2/test.dataset"));
+
+            wrTrain2.write((Integer.toString(entriesForTraining)));
+            wrTrain2.newLine();
+            wrTrain2.write("22 " + getNumberOfClasses());
+            wrTrain2.newLine();
+
+            wrTest2.write((Integer.toString(entriesForTesting)));
+            wrTest2.newLine();
+            wrTest2.write("22 " + getNumberOfClasses());
+            wrTest2.newLine();
+
+            // Init Fold 3 Writer
+            BufferedWriter wrTrain3 = new BufferedWriter(new FileWriter("src/fold3/train.dataset"));
+            BufferedWriter wrTest3 = new BufferedWriter(new FileWriter("src/fold3/test.dataset"));
+
+            wrTrain3.write((Integer.toString(entriesForTraining)));
+            wrTrain3.newLine();
+            wrTrain3.write("22 " + getNumberOfClasses());
+            wrTrain3.newLine();
+
+            wrTest3.write((Integer.toString(entriesForTesting)));
+            wrTest3.newLine();
+            wrTest3.write("22 " + getNumberOfClasses());
+            wrTest3.newLine();
+
+            // Init Fold 4 Writer
+            BufferedWriter wrTrain4 = new BufferedWriter(new FileWriter("src/fold4/train.dataset"));
+            BufferedWriter wrTest4 = new BufferedWriter(new FileWriter("src/fold4/test.dataset"));
+
+            wrTrain4.write((Integer.toString(entriesForTraining)));
+            wrTrain4.newLine();
+            wrTrain4.write("22 " + getNumberOfClasses());
+            wrTrain4.newLine();
+
+            wrTest4.write((Integer.toString(entriesForTesting)));
+            wrTest4.newLine();
+            wrTest4.write("22 " + getNumberOfClasses());
+            wrTest4.newLine();
+
+            int foldIndex = 0;
+
+            int test[] = {0,0,0,0};
+
+            for (Entry entry : entries) {
+                switch (foldIndex) {
+                    case 0:
+                        wrTest.write(entry.entryToString());
+                        wrTest.newLine();
+
+                        wrTrain2.write(entry.entryToString());
+                        wrTrain2.newLine();
+
+                        wrTrain3.write(entry.entryToString());
+                        wrTrain3.newLine();
+
+                        wrTrain4.write(entry.entryToString());
+                        wrTrain4.newLine();
+                        foldIndex++; test[0]++;
+                        break;
+
+                    case 1:
+                        wrTest2.write(entry.entryToString());
+                        wrTest2.newLine();
+
+                        wrTrain.write(entry.entryToString());
+                        wrTrain.newLine();
+
+                        wrTrain3.write(entry.entryToString());
+                        wrTrain3.newLine();
+
+                        wrTrain4.write(entry.entryToString());
+                        wrTrain4.newLine();
+                        foldIndex++; test[1]++;
+                        break;
+
+                    case 2:
+                        wrTest3.write(entry.entryToString());
+                        wrTest3.newLine();
+
+                        wrTrain.write(entry.entryToString());
+                        wrTrain.newLine();
+
+                        wrTrain2.write(entry.entryToString());
+                        wrTrain2.newLine();
+
+                        wrTrain4.write(entry.entryToString());
+                        wrTrain4.newLine();
+                        foldIndex++; test[2]++;
+                        break;
+
+                    case 3:
+                        wrTest4.write(entry.entryToString());
+                        wrTest4.newLine();
+
+                        wrTrain.write(entry.entryToString());
+                        wrTrain.newLine();
+
+                        wrTrain2.write(entry.entryToString());
+                        wrTrain2.newLine();
+
+                        wrTrain3.write(entry.entryToString());
+                        wrTrain3.newLine();
+                        foldIndex = 0; test[3]++;
+                        break;
+                }
+            }
+
+            wrTest.close(); wrTest2.close(); wrTest3.close(); wrTest4.close();
+            wrTrain.close(); wrTrain2.close(); wrTrain3.close(); wrTrain4.close();
+
+            System.out.println("test: " + test[0] + " " + test[1] + " " + test[2] + " " + test[3]);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
+
 
 
     public static void main(String[] args) {
         Parser parserr = new Parser("yay");
 
         System.out.println("Number of classes:" + parserr.getNumberOfClasses());
-        parserr.writeToFile();
+        parserr.writeToFile(true);
     }
 
 }
